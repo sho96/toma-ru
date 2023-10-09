0ae915ee4beadfbc520fc1d44348b16b222d1e25
sho96 <shotaro962008@gmail.com>
2023-10-09 21:26:44 +0900

    fixed bug

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const http = require("http");
 const app = express();
 const httpPort = process.env.PORT || 8000;  // Default to 8000 if no environment variable is set
 
-
 app.use(express.static("public"));
+
+httpServer = http.createServer(app);
+
+const socketServer = new webSocket.Server({ server: httpServer });
+const clients = [];
+
+socketServer.on("connection", (client) => {
+    console.log("connected");
+    client.send("connected");
+    client.on("close", () => console.log("disconnected"));
+    client.on("message", data => {
+        console.log(`client: ${data}`);
+    })
+    client.onerror = () => {
+        console.log("error occurred with client");
+    }
+})
 
 app.get("/", (request, response) => {
     response.status(200).send(readFileSync("./index.html", {encoding: "utf-8"}));
@@ -27,21 +43,5 @@ app.post("/brake", (req, resp) => {
     })
 });
 
-httpServer = http.createServer(app);
 
 httpServer.listen(httpPort, () => console.log(`Server has started listening on port ${httpPort}.`));
-
-const socketServer = new webSocket.Server({ httpServer });
-const clients = [];
-
-socketServer.on("connection", (client) => {
-    console.log("connected");
-    client.send("connected");
-    client.on("close", () => console.log("disconnected"));
-    client.on("message", data => {
-        console.log(`client: ${data}`);
-    })
-    client.onerror = () => {
-        console.log("error occurred with client");
-    }
-})
